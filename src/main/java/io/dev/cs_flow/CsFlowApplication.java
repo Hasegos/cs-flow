680f5e4f81f16fbc7c0a0acf7a3694304d1be17e
Hasegos <csw020106@naver.com>
2026-05-06 16:14:59 +0900

    Initial Commit

---
+++ src/main/java/io/dev/cs_flow/CsFlowApplication.java
@@ -0,0 +1,13 @@
+package io.dev.cs_flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/dev/cs_flow/CsFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dev.cs_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
