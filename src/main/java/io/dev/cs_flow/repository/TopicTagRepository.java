a915d1bf226d318ead2111ebd1e0c275ab273e25
Hasegos <csw020106@naver.com>
2026-09-26 01:27:54 +0900

    FIX TopicTagRepository 수정
    과목에 속한 공개 토픽의 태그별 토픽수 집계 쿼리문 추가

--- src/main/java/io/dev/cs_flow/repository/TopicTagRepository.java
+++ src/main/java/io/dev/cs_flow/repository/TopicTagRepository.java
@@ -1,5 +1,6 @@
 package io.dev.cs_flow.repository;
 
+import io.dev.cs_flow.dto.TagCount;
 import io.dev.cs_flow.model.TopicTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,7 +11,7 @@ import java.util.List;
 /**
  * 토픽 태그(TopicTag) 엔티티에 대한 데이터 접근 계층.
  * <p>
- * 특정 토픽에 연결된 태그 목록 조회 기능을 제공한다.
+ * 특정 토픽에 연결된 태그 목록 조회, 과목별 태그 집계 기능을 제공한다.
  * </p>
  */
 public interface TopicTagRepository extends JpaRepository<TopicTag, Long> {
@@ -26,4 +27,22 @@ public interface TopicTagRepository extends JpaRepository<TopicTag, Long> {
             WHERE tt.topic.topicId = :topicId
             """)
     List<TopicTag> findTagsByTopicId(@Param("topicId") Long topicId);
+
+    /**
+     * 과목에 속한 공개 토픽의 태그별 토픽 수를 집계한다.
+     *
+     * @param subjectSlug 과목 영문 식별자
+     * @return 태그와 토픽 수 목록 (토픽 수 내림차순, 태그 오름차순)
+     */
+    @Query("""
+            SELECT new io.dev.cs_flow.dto.TagCount(tt.tag, COUNT(tt))
+            FROM TopicTag tt
+            JOIN tt.topic t
+            JOIN t.subject s
+            WHERE s.slug = :subjectSlug
+            AND t.isPublished = true
+            GROUP BY tt.tag
+            ORDER BY COUNT(tt) DESC, tt.tag ASC
+            """)
+    List<TagCount> countPublishedTagsBySubjectSlug(@Param("subjectSlug") String subjectSlug);
 }
